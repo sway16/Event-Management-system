4e70c77c8506272ef84416bf49b6017b62edf674
sway16 <dawsingsh@gmail.com>
2023-11-24 14:42:23 +0600

    update 1

--- lib/silder/silder_screen.dart
+++ lib/silder/silder_screen.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:eventmanagement/Screens/screen-1.dart';
 import 'package:flutter/material.dart';
 
 class Silder_Screen extends StatefulWidget {
@@ -20,7 +21,7 @@ class _Silder_ScreenState extends State<Silder_Screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green.shade600,
+        backgroundColor: Colors.transparent,
         centerTitle: true,
         title: Text('Silder'),
         leading: IconButton(onPressed: (){},
@@ -90,90 +91,105 @@ class _Silder_ScreenState extends State<Silder_Screen> {
             ),
           ),
           Expanded(
-            flex: 7,
+            flex: 6,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      height:180,
-                      width: 180,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.green,
-                      ),
-                      alignment: Alignment.center,
-                      child: Text(" Exam Notice",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          fontSize: 20,
+
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> Screen1()));},
+                        child: Container(
+                          height:170,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green,
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Exam Notice",  textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      height:180,
-                      width: 180,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.green,
-                      ),
-                      alignment: Alignment.center,
-                      child: Text(
-                        "Department Events",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20
+                      TextButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> Screen1()));},
+                        child: Container(
+                          height:170,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green,
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Department Events",  textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      height:180,
-                      width: 180,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.green,
-                      ),
-                      alignment: Alignment.center,
-                      child: Text(" Club Events",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> Screen1()));},
+                        child: Container(
+                          height:170,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green,
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Exam Notice",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      height:180,
-                      width: 180,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.green,
-                      ),
-                      alignment: Alignment.center,
-                      child: Text("University Program",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20
+                      TextButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> Screen1()));},
+                        child: Container(
+                          height:170,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green,
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Department Events",  textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
                   ],
                 ),
             ),
-
-
         ],
       ),
     );
